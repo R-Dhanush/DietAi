1469426476f0065972c497d6019f037b7ff03d26
R-Dhanush <154717055+R-Dhanush@users.noreply.github.com>
2025-04-14 08:44:59 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://dietai-backend.onrender.com' || 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json'
   },
